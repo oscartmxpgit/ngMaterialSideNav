f7e165fdaa844fa0c7af91c8fb980d64f046d116
oscartmxpgit <92880098+oscartmxpgit@users.noreply.github.com>
2022-02-08 20:36:50 +0100

    get from api

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public  http :HttpClient) { }
+
+  res:any;
+  info_calendar:any;
 
   ngOnInit(): void {
-  }
 
+    this.http.get('http://calapi.inadiutorium.cz/api/v0/la/calendars/general-en/today').subscribe((res)=>{
+            console.log(res);
+            this.info_calendar= res;
+        },
+        error => {
+          console.error('Request failed with error')
+
+        }
+        );
+      }
 }
